083a6e5113d4d4e8d980cbca351ab67da33aa24e
sh_bang <eztodj23@gmail.com>
2017-02-10 14:37:14 +0900

    first commit

---
+++ src/Hello_world.java
@@ -0,0 +1,16 @@
+/**
+ * Created by SH_BANG on 2017-02-10.
+ */
+
+import java.util.Scanner;
+
+public class Hello_world {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+
+        System.out.println("Hello, world!");
+
+    }
+
+}
